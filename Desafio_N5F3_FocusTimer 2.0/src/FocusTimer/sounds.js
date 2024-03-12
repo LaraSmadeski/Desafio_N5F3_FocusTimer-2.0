5bd4457372196034c031530e4787dc26cd068820
Lara Smadeski <136000289+LaraSmadeski@users.noreply.github.com>
2024-03-12 12:27:30 +0000

    Add files via upload

---
+++ Desafio_N5F3_FocusTimer 2.0/src/FocusTimer/sounds.js
@@ -0,0 +1,9 @@
+export const forest = new Audio('./assets/Floresta.wav')
+
+export const rain = new Audio('./assets/Chuva.wav')
+
+export const coffeeShop = new Audio('./assets/Cafeteria.wav')
+
+export const fireplace = new Audio('./assets/Lareira.wav')
+
+bgAudio.loop = true
